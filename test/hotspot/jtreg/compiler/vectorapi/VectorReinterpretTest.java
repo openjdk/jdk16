94350c09752bbd7a8ebbcd055f960cd3e16848e0
Jie Fu <jiefu@openjdk.org>
2021-01-19 11:52:06 +0800

    Refine the test to always reproduce the bug

--- test/hotspot/jtreg/compiler/vectorapi/VectorReinterpretTest.java
+++ test/hotspot/jtreg/compiler/vectorapi/VectorReinterpretTest.java
@@ -30,16 +30,28 @@ import jdk.incubator.vector.VectorSpecies;
 /*
  * @test
  * @bug 8259775
- * @summary test code-gen for VectorReinterpret operation for 512-bit vectors
+ * @summary Incorrect code-gen for VectorReinterpret operation
  * @modules jdk.incubator.vector
- * @run main compiler.vectorapi.VectorReinterpretTest512
+ * @run main/othervm -Xbatch compiler.vectorapi.VectorReinterpretTest
  */
 
-public class VectorReinterpretTest512 {
+public class VectorReinterpretTest {
+    static final VectorSpecies<Byte> SPECIES_128 = ByteVector.SPECIES_128;
     static final VectorSpecies<Byte> SPECIES_256 = ByteVector.SPECIES_256;
     static final VectorSpecies<Byte> SPECIES_512 = ByteVector.SPECIES_512;
 
     static byte[] a = new byte[64];
+
+    private static void test256_128_256() {
+        ByteVector av = ByteVector.fromArray(SPECIES_256, a, 0);
+        ByteVector bv = (ByteVector)av.reinterpretShape(SPECIES_128, 0);
+        ByteVector cv = (ByteVector)bv.reinterpretShape(SPECIES_256, 0);
+
+        if (bv.reduceLanes(VectorOperators.ADD) != 16 ||
+            cv.reduceLanes(VectorOperators.ADD) != 16) {
+            throw new Error("Failed");
+        }
+    }
 
     private static void test512_256_512() {
         ByteVector av = ByteVector.fromArray(SPECIES_512, a, 0);
@@ -58,6 +70,7 @@ public class VectorReinterpretTest512 {
         }
 
         for (int i = 0; i < 100000; i++) {
+            test256_128_256();
             test512_256_512();
         }
     }
